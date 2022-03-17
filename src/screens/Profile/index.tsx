29a47f3c23e1f3b9d8edae4cc00b232fce5b9b43
Bruno Becoski <brunobecoski@gmail.com>
2022-03-16 22:39:00 -0300

    Testes do Profile

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Button,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+export function Profile() {
+  return (
+    <View>
+      <Text testID="text-title">
+        Perfil
+      </Text>
+
+      <TextInput 
+        testID="input-name"
+        placeholder="Nome"
+        autoCorrect={false}
+        value="Bruno"
+      />
+
+      <TextInput
+        testID="input-surname"
+        placeholder="Sobrenome"
+        value="Becoski"
+      />
+
+      <Button 
+        title="Salvar"
+        onPress={() => {}}
+      />
+    </View>
+  )
+}
